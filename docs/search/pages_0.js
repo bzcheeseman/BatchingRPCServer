f89544be001534474d446b870b1b7521cccacbfd
Aman <bzcheeseman@gmail.com>
2017-12-27 12:26:01 -0500

    update docs

--- docs/search/pages_0.js
+++ docs/search/pages_0.js
@@ -1,4 +1,4 @@
 var searchData=
 [
-  ['batchingrpcserver',['BatchingRPCServer',['../md___users__aman_code__batching_r_p_c_server__r_e_a_d_m_e.html',1,'']]]
+  ['batchingrpcserver',['BatchingRPCServer',['../index.html',1,'']]]
 ];
